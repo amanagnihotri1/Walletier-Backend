bb8e7331185ea6bf5c828baa316150eec49ec58b
amanagnihotri1 <01mragnihotri@gmail.com>
2026-08-14 01:57:06 +0530

    Fix:Fixing error  handelling  function issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const compression = require('compression')
 const helmet = require('helmet')
+const { errorHandler } = require("./helper/utils");
 const{rateLimit, MINUTE}=require("express-rate-limit");
 require('dotenv').config();
 const express=require("express");
